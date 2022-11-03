14fc30ca5e47b14bb43c33627e76e9e64f5eea72
Waris Agung Widodo <ido.alit@gmail.com>
2022-11-03 22:24:20 +0700

    Add bootstraper file

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager;
+
+// create new instance of capsule manager
+$manager = new Manager();
+
+// add SLiMS default connection
+$manager->addConnection([
+    "driver" => DB_DRIVER ?? 'mysql',
+    "host" => DB_HOST,
+    "port" => DB_PORT,
+    "database" => DB_NAME,
+    "username" => DB_USERNAME,
+    "password" => DB_PASSWORD
+]);
+
+// set connection as global
+$manager->setAsGlobal();
+$manager->bootEloquent();
