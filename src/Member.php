fd9aed2c480c55b77eccb5cf41c64b211aa237f4
Waris Agung Widodo <ido.alit@gmail.com>
2022-12-04 14:48:21 +0700

    Update member models

--- src/Member.php
+++ src/Member.php
@@ -19,6 +19,13 @@ class Member extends Model {
     protected $primaryKey = 'member_id';
 
     /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['member_id'];
+
+    /**
      * Indicates if the model's ID is auto-incrementing.
      *
      * @var bool
